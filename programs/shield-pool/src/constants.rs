47edf6da5d7c54ca675dad2fbe2dedb9dc4d791d
vict0rcarvalh0 <victordecarvalho342@gmail.com>
2025-12-26 22:14:10 -0300

    chore(constants): update WITHDRAW_VKEY_HASH to new value for shield-pool

--- programs/shield-pool/src/constants.rs
+++ programs/shield-pool/src/constants.rs
@@ -2,7 +2,7 @@ use five8_const::decode_32_const;
 
 /// SP1 Withdraw Circuit VKey Hash
 pub const WITHDRAW_VKEY_HASH: &str =
-    "0x00321aa2a362bcc8a7005f02ddf788fa60922292b9dc0cfd1f71e3a2ce4a9b92";
+    "0x00082602a6f78681c6165ebb9aa0e64a190116f839916c495fca67ba665f424f";
 
 pub const ADMIN_AUTHORITY: [u8; 32] =
     decode_32_const("mgfSqUe1qaaUjeEzuLUyDUx5Rk4fkgePB5NtLnS3Vxa");
